550053b74e2192157ab7a143d23b9e83a56d674f
Tatyana24121996 <31986590+Tatyana24121996@users.noreply.github.com>
2017-09-21 12:54:59 +0200

    Update creatable.php

--- creatable.php
+++ creatable.php
@@ -2,13 +2,25 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:sherstyankina.database.windows.net,1433; Database = Проектный практикум", "Tanya", "{your_password_here}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE registration_tbl(
+    id INT NOT NULL IDENTITY(1,1) 
+    PRIMARY KEY(id),
+    name VARCHAR(30),
+    email VARCHAR(30),
+    date DATE)";
+    $conn->query($sql);
 }
+catch(Exception $e){
+    die(print_r($e));
+}
+echo "<h3>Table created.</h3>";
+?>
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "Tanya@sherstyankina", "pwd" => "{your_password_here}", "Database" => "Проектный практикум", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "Tanya@sherstyankina", "pwd" => {Sobachka1}, "Database" => "Проектный практикум", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:sherstyankina.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
