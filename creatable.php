92267838cebd4e875e3e7dc9b85cca090f711eb0
Tatyana24121996 <31986590+Tatyana24121996@users.noreply.github.com>
2017-11-07 09:34:32 +0200

    Update creatable.php

--- creatable.php
+++ creatable.php
@@ -1,26 +1,17 @@
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:sherstyankina.database.windows.net,1433; Database = Проектный практикум", "Tanya", "{your_password_here}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE TABLE registration_tbl(
-    id INT NOT NULL IDENTITY(1,1) 
-    PRIMARY KEY(id),
-    name VARCHAR(30),
-    email VARCHAR(30),
-    date DATE)";
-    $conn->query($sql);
-}
-catch(Exception $e){
-    die(print_r($e));
-}
-echo "<h3>Table created.</h3>";
+<?php 
+try { 
+$conn = new PDO("sqlsrv:server = tcp:pinyasova.database.windows.net,1433; Database = Progr", "sherstyankina.96", "qwerty123456"); 
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+$sql = "CREATE TABLE registration_tbl1( 
+id INT NOT NULL IDENTITY(1,1) 
+PRIMARY KEY(id), 
+name VARCHAR(30), 
+email VARCHAR(30), 
+date DATE)"; 
+$conn->query($sql); 
+} 
+catch (PDOException $e) { 
+print("Error connecting to SQL Server."); 
+die(print_r($e)); 
+} 
 ?>
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "Tanya@sherstyankina", "pwd" => "qwerty123456", "Database" => "Проектный практикум", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:sherstyankina.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
