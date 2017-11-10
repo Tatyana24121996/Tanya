2f81fb229d4507fce4728e768add63566d204f03
Tatyana24121996 <31986590+Tatyana24121996@users.noreply.github.com>
2017-11-10 12:51:01 +0200

    Update creatable.php

--- creatable.php
+++ creatable.php
@@ -1,17 +1,17 @@
-<?php 
-try { 
-$conn = new PDO("sqlsrv:server = tcp:pinyasova.database.windows.net,1433; Database = Progr", "sherstyankina.96", "qwerty123456"); 
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-$sql = "CREATE TABLE registration_tbl1( 
-id INT NOT NULL IDENTITY(1,1) 
-PRIMARY KEY(id), 
-name VARCHAR(30), 
-email VARCHAR(30), 
-date DATE)"; 
-$conn->query($sql); 
-} 
-catch (PDOException $e) { 
-print("Error connecting to SQL Server."); 
-die(print_r($e)); 
-} 
+<?php
+try {
+$conn = new PDO("sqlsrv:server = tcp:pinyasova.database.windows.net,1433; Database = Progr", "Valera", "hswfhmlyz08");
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql = "CREATE TABLE registration_tbl1(
+id INT NOT NULL IDENTITY(1,1)
+PRIMARY KEY(id),
+name VARCHAR(30),
+email VARCHAR(30),
+date DATE)";
+$conn->query($sql);
+}
+catch (PDOException $e) {
+print("Error connecting to SQL Server.");
+die(print_r($e));
+}
 ?>
